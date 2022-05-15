6fccdc3309f93fb1244e0b234626a0ef71cf6f60
Rohit Jangid <>
2022-05-15 17:10:33 +0530

    matrixRotation.cpp optimized

--- matrixRotation.cpp
+++ matrixRotation.cpp
@@ -6,20 +6,13 @@ public:
     vector<vector<int> > rotateMatrix(vector<vector<int>> &matrix){
         int r = matrix.size(), c = matrix[0].size();
         vector<vector<int>> rotatedMatrix(c, vector<int>(r));
-        if(r == c) {
-            for(int i = 0; i < r; i++) {
-                for(int j = 0; j <= i; j++) {
-                    rotatedMatrix[i][j] = matrix[j][i];
-                    rotatedMatrix[j][i] = matrix[i][j];
-                }
-            }
-        } else {
-            for(int i = 0; i < c; i++) {
-                for(int j = 0; j < r; j++) {
-                    rotatedMatrix[i][j] = matrix[j][i];
-                }
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                rotatedMatrix[j][i] = matrix[i][j];
             }
         }
+
         for(int i = 0; i < c; i++) {
             std::reverse(rotatedMatrix[i].begin(), rotatedMatrix[i].end());
         }
